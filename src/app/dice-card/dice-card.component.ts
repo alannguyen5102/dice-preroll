e688d2cb8f876b7ab16bd908fd9da0c89f52577c
Alan Nguyen <aknguyen@scu.edu>
2021-04-09 16:03:31 -0700

    Template for each die type

--- src/app/dice-card/dice-card.component.ts
+++ src/app/dice-card/dice-card.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {   
+  ComponentRef,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  ViewChild,
+  ViewRef, 
+  Component, 
+  OnInit, 
+  Input} from '@angular/core';
+import { RollCardComponent } from '../roll-card/roll-card.component';
 
 @Component({
   selector: 'app-dice-card',
@@ -7,7 +16,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiceCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() public dieType: number;
+
+  @ViewChild("viewContainerRef", { read: ViewContainerRef })
+  VCR: ViewContainerRef;
+
+  child_unique_key: number = 0;
+  componentsReferences = Array<ComponentRef<RollCardComponent>>()
+
+  constructor(private CFR: ComponentFactoryResolver) {}
+
+  createComponent(result: number) {
+    let componentFactory = this.CFR.resolveComponentFactory(RollCardComponent);
+
+    let childComponentRef = this.VCR.createComponent(componentFactory);
+
+    let childComponent = childComponentRef.instance;
+    childComponent.unique_key = ++this.child_unique_key;
+    childComponent.rollResult = result;
+    childComponent.parentRef = this;
+    
+
+    // add reference for newly created component
+    this.componentsReferences.push(childComponentRef);
+  }
+
+  remove(key: number) {
+    console.log("remove key: " + key);
+    if (this.VCR.length < 1) return;
+
+    let componentRef = this.componentsReferences.filter(
+      x => x.instance.unique_key == key
+    )[0];
+
+
+    console.log("componentRef" + componentRef);
+
+    let vcrIndex: number = this.VCR.indexOf(componentRef as any);
+
+    // removing component from container
+    console.log("Removing: " + vcrIndex);
+    this.VCR.remove(vcrIndex);
+
+    // removing component from the list
+    this.componentsReferences = this.componentsReferences.filter(
+      x => x.instance.unique_key !== key
+    );
+  }
 
   ngOnInit(): void {
   }
--- src/app/roll-card/roll-card.component.ts
+++ src/app/roll-card/roll-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DiceCardComponent } from '../dice-card/dice-card.component';
 
 @Component({
   selector: 'app-roll-card',
   templateUrl: './roll-card.component.html',
   styleUrls: ['./roll-card.component.scss']
 })
-export class RollCardComponent implements OnInit {
+export class RollCardComponent {
+
+  public unique_key: number;
+  public parentRef: DiceCardComponent;
+  public rollResult: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  remove_me() {
+    console.log("key: " + this.unique_key)
+    this.parentRef.remove(this.unique_key)
+  }
 }
