a3d26117a98dc17d85a05506f2ae5cf4d0c06868
Alan Nguyen <aknguyen@scu.edu>
2021-04-09 14:16:53 -0700

    Adding roll-card

---
+++ src/app/roll-card/roll-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roll-card',
+  templateUrl: './roll-card.component.html',
+  styleUrls: ['./roll-card.component.scss']
+})
+export class RollCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
