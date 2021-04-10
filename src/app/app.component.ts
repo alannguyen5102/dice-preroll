5e89ac504167e08a1eb74b2660fdb2eb9a370b4c
Alan Nguyen <aknguyen@scu.edu>
2021-04-09 18:21:03 -0700

    Adding clear all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dice-preroll';
+
+  autoRoll() {
+    for(var i:number = 1; i < 10; i++) {
+      document.getElementById("d100").click();
+      document.getElementById("d20").click();
+      document.getElementById("d12").click();
+
+      document.getElementById("d10").click();
+      document.getElementById("d8").click();
+      document.getElementById("d6").click();
+
+      document.getElementById("d4").click();
+      document.getElementById("d3").click();
+      document.getElementById("d2").click();
+    }
+  }
+
+  clearAll() {
+    document.getElementById("c100").click();
+    document.getElementById("c20").click();
+    document.getElementById("c12").click();
+
+    document.getElementById("c10").click();
+    document.getElementById("c8").click();
+    document.getElementById("c6").click();
+
+    document.getElementById("c4").click();
+    document.getElementById("c3").click();
+    document.getElementById("c2").click();
+    }
+  
 }
